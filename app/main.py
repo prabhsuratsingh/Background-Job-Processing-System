61030639aee675d7d0b77dc02a7c2945c30abc1c
prabhsuratsingh <prabhsurat17@gmail.com>
2026-01-05 12:57:42 +0530

    fix api and docker

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
 from contextlib import asynccontextmanager
+from uuid import UUID, uuid4
 from fastapi import FastAPI, Depends, HTTPException
 import redis
 from sqlalchemy import text
 from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm import Session
 
-from database import db
+from app.database import db
+from app.database.crud import fetch_job, insert_job, update_job_status
+from app.schemas.job import JobCreate, JobCreateAPI
+from app.workers import celery_app
+from app.workers.tasks import celery_task
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -29,7 +34,6 @@ async def health_redis():
             "Redis": "Unhealthy",
             "Error": redis.exceptions.ConnectionError.__name__
         })
-    
 
 @app.get("/health/postgres")
 def health_postgres(db: Session = Depends(db.get_db)):
@@ -42,6 +46,36 @@ def health_postgres(db: Session = Depends(db.get_db)):
             "Postgres": "Unhealthy",
             "Error": OperationalError.__name__
         })
+
+
+@app.post("/job/create")
+async def create_job(payload: JobCreateAPI, db: Session = Depends(db.get_db)):
+    job_id = uuid4()
+
+    job = JobCreate(
+        id=job_id,
+        status="PENDING",
+        job_type=payload.job_type,
+        input_data=payload.payload
+    )
+
+    insert_job(db, job)
+
+    celery_task.delay(str(job_id))
+
+    update_job_status(db, job_id, new_status="QUEUED")
+
+    return {
+        "job id" : job_id
+    }
+
+@app.get("/jobs/fetch/{job_id}")
+def get_job(job_id: UUID, db: Session = Depends(db.get_db)):
+    resp = fetch_job(db, job_id)
+    return {
+        "data": resp
+    }
+
 
 if __name__ == "__main__":
 
